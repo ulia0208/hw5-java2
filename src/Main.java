3a32e72decbee0e96d854ff1ca36237fe102565e
ulia0208 <ulia0208@yandex.ru>
2023-05-19 19:23:04 +0300

    kuku

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,161 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //hw0();
+
+        String lineN = "Иван Иванов, Светлана Петрова, Кристина Белова, \n"+
+                "Анна Мусина, Анна Крутова, Иван Юрин, Петр Лыков, \n"+
+                "Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, \n"+
+                "Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов.\n";
+
+        hw1(lineN);
+
+
+
     }
+
+    static void hw0(){
+        //Реализуйте структуру телефонной книги с помощью HashMap,
+        //учитывая, что 1 человек может иметь несколько телефонов.
+
+        PhoneBook phoneBook=new PhoneBook();
+        Scanner scanner=new Scanner(System.in);
+
+        while (true){
+            System.out.println("Нажмите 1 для добавления записи и 2 для просмотра справочника ");
+            int choice=scanner.nextInt();
+            if(choice==1){
+                System.out.println("Введите имя ");
+                String name=scanner.next();
+                System.out.println("Введите номер ");
+                String num = scanner.next();
+                phoneBook.addNum(name, num);
+            }else if (choice==2){
+                phoneBook.printAll();
+            }
+        }
+    }
+
+    static void hw1(String lineN){
+                /*Пусть дан список сотрудников:
+                Иван Иванов, Светлана Петрова, Кристина Белова,
+                Анна Мусина, Анна Крутова, Иван Юрин, Петр Лыков,
+                Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина,
+                Мария Рыкова, Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов.
+                Написать программу, которая найдет и выведет повторяющиеся имена с количеством повторений.
+                Отсортировать по убыванию популярности. Для сортировки использовать TreeMap.*/
+
+        lineN=lineN.replace(",", "");
+        lineN=lineN.replace(".", "");
+
+        lineN=lineN.replace("\n", "");
+
+
+        String[] namesSurnames=lineN.split(" ");
+
+        int length=0;
+        int j=0;
+
+        for (int i = 0; i < namesSurnames.length; i++) {
+            if(i%2==0){
+                length++;
+            }
+
+        }
+        List<String> listNames=new ArrayList<>();
+
+        for (int i = 0; i < namesSurnames.length; i++){
+            if(i%2==0){
+                listNames.add(namesSurnames[i]);
+            }
+
+        }
+
+        System.out.println("list "+listNames);
+
+       Map<Integer,String>mapName = createMap(listNames);
+
+//        Map<List<Integer>,String>mapName = new TreeMap<>();
+//        for (int i = 0; i < mapName.size(); i++) {
+//            int count = 0;
+//
+//            for (int h = 0; h < mapName.size(); h++) {
+//                if (mapName.get(i).equals(mapName.get(h))) {
+//                    count++;
+//                }
+//            }
+//
+//            if (mapName.containsValue(mapName.get(i)) == false) {
+//
+//                List<Integer> list = new ArrayList<>();
+//                list.add(count);
+//
+//                mapName.put(list, mapName.get(i));
+//
+//            }
+//        }
+
+
+
+        System.out.println(mapName);
+    }
+
+    public static Map<Integer,String> createMap(List<String> name){
+        Map<Integer, String> mapName = new HashMap<>();
+        for (int i = 0; i < name.size(); i++) {
+            int count=0;
+            for (int j = 0; j < name.size(); j++) {
+                if(name.get(i).equals(name.get(j))){
+                    count++;
+                }
+            }
+            if(mapName.containsValue(name.get(i))==false){
+                mapName.put(count,name.get(i));
+
+            }
+        }
+        return mapName;
+    }
+//public static Map<List<Integer>,String> createMap(List<String> name){
+//    Map<List<Integer>, String> mapName = new HashMap<>();
+//    for (int i = 0; i < name.size(); i++) {
+//        int count=0;
+//        for (int j = 0; j < name.size(); j++) {
+//            if(name.get(i).equals(name.get(j))){
+//                count++;
+//            }
+//        }
+//
+//
+//        if(mapName.containsValue(name.get(i))==false){
+//
+//            List<Integer> list = new ArrayList<>();
+//            list.add(count);
+//
+//            mapName.put(list,name.get(i));
+//
+//        }
+//    }
+//    return mapName;
+//}
+//    public static Map<String,Integer> createMap(List<String> name){
+//        Map<String, Integer> mapName = new HashMap<>();
+//        for (int i = 0; i < name.size(); i++) {
+//            int count=0;
+//            for (int j = 0; j < name.size(); j++) {
+//                if(name.get(i).equals(name.get(j))){
+//                    count++;
+//                }
+//            }
+//            if(mapName.containsValue(name.get(i))==false){
+//                mapName.put(name.get(i), count);
+//
+//            }
+//        }
+//        return mapName;
+//    }
+
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class PhoneBook {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PhoneBook {
+    private HashMap<String, List>contacts;
+
+    public PhoneBook(){
+        contacts=new HashMap<String,List>();
+    }
+
+
+    public void addNum(String name, String number){
+        List numbers;
+        if(contacts.containsKey(name)) {
+            numbers = contacts.get(name);
+        }else{
+            numbers=new ArrayList();
+            contacts.put(name,numbers);
+        }
+        numbers.add(number);
+    }
+
+    public void printAll(){
+        for(String name: contacts.keySet()){
+            System.out.println(name+":");
+            List numbers = contacts.get(name);
+            for(Object num: numbers) {
+                System.out.println(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
